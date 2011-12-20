e8bc3c77fee3fe19c2838b86848f0b9309b49cff
Shaun Thompson <neko68k@gmail.com>
2011-12-19 22:45:21 -0700

    Made some changes to the notification. Cleaned up the notification code also.

--- src/com/neko68k/M1/PlayerService.java
+++ src/com/neko68k/M1/PlayerService.java
@@ -1,6 +1,6 @@
 package com.neko68k.M1;
 
-import android.R;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,6 +8,10 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.widget.RemoteViews;
+
+//import android.R;
+import com.neko68k.emu.M1Android.R;
 
 // this will handle all the threading and shit
 // so we can properly stop it and allow the app
@@ -19,7 +23,7 @@ public class PlayerService extends Service{
 	
 	private int NOTIFICATION = 1;
 	String text;
-	Notification notification;
+	Notification notification = null;
 	PendingIntent contentIntent;
 	
 	AudioDevice ad = new AudioDevice("deviceThread");	
@@ -53,48 +57,51 @@ public class PlayerService extends Service{
 		stopForeground(true);
 	}
 	public void play(){
-		notification=new Notification(R.drawable.ic_media_play,
-                "",
-                System.currentTimeMillis());
-		Intent i=new Intent(this, M1Android.class);
-		
-		i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|
-		Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		
-		PendingIntent pi=PendingIntent.getActivity(this, 0,
-		                        i, 0);
-		
-		//notification.setLatestEventInfo(this, "M1 Android",
-		    //"Now Playing: \"Ummmm, Nothing\"",
-		    //pi);
+				
+		if(notification != null)
+			notification=new Notification();
+
 		setNoteText();
-		notification.flags|=Notification.FLAG_NO_CLEAR;
-		
-		startForeground(1337, notification);
-		ad.PlayStart();
-		//showNotification();
+		ad.PlayStart();		
 	}
 	
 	public void setNoteText(){
 
-		text = NDKBridge.getGameTitle(NDKBridge.curGame).getText()+"\n"+NDKBridge.getSong(NDKBridge.getCurrentCmd());
-		if(text==null){
-			text = NDKBridge.getGameTitle(NDKBridge.curGame).getText();
+		notification.flags|=Notification.FLAG_NO_CLEAR;
+		RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.custom_notification);
+		contentView.setImageViewResource(R.id.image, R.drawable.icon);
+		contentView.setTextViewText(R.id.title, "M1Android");
+		
+		
+		notification.contentView = contentView;
+		
+		
+		text = NDKBridge.getSong(NDKBridge.getCurrentCmd());
+		if(text!=null){
+			contentView.setTextViewText(R.id.text, text);
+			contentView.setTextViewText(R.id.text2, NDKBridge.getGameTitle(NDKBridge.curGame).getText());
 		}
+		if(text==null){
+			contentView.setTextViewText(R.id.text, "No track list");
+			contentView.setTextViewText(R.id.text2, NDKBridge.getGameTitle(NDKBridge.curGame).getText()); 
+		}
+		
+		
 
 		contentIntent = PendingIntent.getActivity(this, 0, 
 				new Intent(this, M1Android.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|
 		Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 		
-		notification.setLatestEventInfo(this, "M1 Android", text, contentIntent);
-		startForeground(1337, notification);
-		//notification.flags = Notification.FLAG_NO_CLEAR|Notification.DEFAULT_SOUND|Notification.DEFAULT_VIBRATE|Notification.DEFAULT_LIGHTS;
-		//mNM.notify(NOTIFICATION, notification);		
+		Intent notificationIntent = new Intent(this, M1Android.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		notification.contentIntent = contentIntent;
+				
+		startForeground(1337, notification);		
 	}
 	
 	//@Override
 	public void onDestory(){
-		mNM.cancelAll();//(NOTIFICATION);
+		stopForeground(true);
 		ad.PlayQuit();
 	}
 	
@@ -104,12 +111,6 @@ public class PlayerService extends Service{
 	}
 	
 	private final IBinder mBinder = new LocalBinder();
-	
-	private void showNotification(){
-		
-		 //notification = new Notification(R.drawable.ic_media_play, text, System.currentTimeMillis());
-		 setNoteText();
-	}
 	
 	
 	
