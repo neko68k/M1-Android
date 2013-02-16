4be5f96d4fc839777b05934e983c51efd94133ba
Shaun Thompson <neko68k@gmail.com>
2013-02-15 17:59:20 -0700

    added proper sized icons.

---
+++ gen/com/neko68k/emu/M1Android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.neko68k.emu.M1Android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- gen/com/neko68k/emu/M1Android/R.java
+++ gen/com/neko68k/emu/M1Android/R.java
@@ -16,7 +16,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
+        public static final int ic_stat_example=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
         public static final int board=0x7f0a0006;
