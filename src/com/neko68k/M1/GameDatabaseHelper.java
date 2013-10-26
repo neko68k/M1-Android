34700ab66a1f42267dbaa14df3b4d7904db657e1
Shaun Thompson <neko68k@gmail.com>
2013-10-25 20:41:16 -0600

    fixed up table column name constants

---
+++ src/com/neko68k/M1/GameDatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.neko68k.M1;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class GameDatabaseHelper extends SQLiteOpenHelper {
+
+  private static final String DATABASE_NAME = "m1db";
+  private static final int DATABASE_VERSION = 1;
+
+  public GameDatabaseHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+
+  // Method is called during creation of the database
+  @Override
+  public void onCreate(SQLiteDatabase database) {
+    GameListOpenHelper.onCreate(database);
+  }
+
+  // Method is called during an upgrade of the database,
+  // e.g. if you increase the database version
+  @Override
+  public void onUpgrade(SQLiteDatabase database, int oldVersion,
+      int newVersion) {
+	  GameListOpenHelper.onUpgrade(database, oldVersion, newVersion);
+  }
+}
+ 
--- src/com/neko68k/M1/GameListOpenHelper.java
+++ src/com/neko68k/M1/GameListOpenHelper.java
@@ -1,43 +1,52 @@
 package com.neko68k.M1;
 
 import android.content.ContentValues;
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 
 
-public class GameListOpenHelper extends SQLiteOpenHelper {
- 
+
+
+
+public class GameListOpenHelper {
+	public static final String KEY_ID = "_id";
+	public static final String KEY_TITLE = "title";
+	public static final String KEY_YEAR = "year";
+	public static final String KEY_ROMNAME = "romname";
+	public static final String KEY_MFG = "mfg";
+	public static final String KEY_SYS = "sys";
+	public static final String KEY_CPU = "cpu";
+	public static final String KEY_SOUND1 = "sound1";
+	public static final String KEY_SOUND2 = "sound2";
+	public static final String KEY_SOUND3 = "sound3";
+	public static final String KEY_SOUND4 = "sound4";
+	public static final String KEY_SOUND5 = "sound5";
+	public static final String KEY_LISTAVAIL = "sound5";
 
 	private static final int DATABASE_VERSION = 1;
     private static final String GAMELIST_TABLE_NAME = "gamelist";
     private static final String GAMELIST_TABLE_CREATE =
                 "CREATE TABLE " + GAMELIST_TABLE_NAME + " (" +
-                "index" + " INTEGER PRIMARY KEY, " +
-                "title" + " TEXT, " +
-                "year" + " TEXT, " +
-                "romname" + " TEXT, " +
-                "mfg" + " TEXT, " +
-                "sys" + " TEXT, " +
-                "cpu" + " TEXT, " +
-                "sound1" + " TEXT, " +
-                "sound2" + " TEXT, " +
-                "sound3" + " TEXT, " +
-                "sound4" + " TEXT, " +
-                "sound5" + " TEXT, " +
-                "listavail" + " INTEGER);";
+                KEY_ID + " INTEGER PRIMARY KEY, " +
+                KEY_TITLE + " TEXT, " +
+                KEY_YEAR + " TEXT, " +
+                KEY_ROMNAME + " TEXT, " +
+                KEY_MFG + " TEXT, " +
+                KEY_SYS + " TEXT, " +
+                KEY_CPU + " TEXT, " +
+                KEY_SOUND1 + " TEXT, " +
+                KEY_SOUND2 + " TEXT, " +
+                KEY_SOUND3 + " TEXT, " +
+                KEY_SOUND4 + " TEXT, " +
+                //"sound5" + " TEXT, " +
+                KEY_LISTAVAIL + " INTEGER);";
 
-    GameListOpenHelper(Context context) {
-        super(context, "M1Android", null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
+    
+    public static void onCreate(SQLiteDatabase db) {
         db.execSQL(GAMELIST_TABLE_CREATE);
     }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	
+	public static void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// // Drop older table if existed
         db.execSQL("DROP TABLE IF EXISTS " + GAMELIST_TABLE_NAME);
  
@@ -45,25 +54,25 @@ public class GameListOpenHelper extends SQLiteOpenHelper {
         onCreate(db);
 	}
 	
-	public void addGame(Game game) {
-	    SQLiteDatabase db = this.getWritableDatabase();
+	public static void addGame(Game game) {
+	    //SQLiteDatabase db = this.getWritableDatabase();
 	 
 	    ContentValues values = new ContentValues();
-	    values.put("index", game.getIndex()); 
-	    values.put("title", game.getTitle()); 
-	    values.put("year", game.getYear()); 
-	    values.put("romname", game.getRomname()); 
-	    values.put("mfg", game.getMfg()); 
-	    values.put("sys", game.getSys()); 
-	    values.put("cpu", game.getTitle()); 
-	    values.put("sound1", game.getTitle()); 
-	    values.put("sound2", game.getTitle()); 
-	    values.put("sound3", game.getTitle()); 
-	    values.put("sound4", game.getTitle()); 	   
-	    values.put("listavail", game.getListavail()); 
+	    values.put(KEY_ID, game.getIndex()); 
+	    values.put(KEY_TITLE, game.getTitle()); 
+	    values.put(KEY_YEAR, game.getYear()); 
+	    values.put(KEY_ROMNAME, game.getRomname()); 
+	    values.put(KEY_MFG, game.getMfg()); 
+	    values.put(KEY_SYS, game.getSys()); 
+	    values.put(KEY_CPU, game.getTitle()); 
+	    values.put(KEY_SOUND1, game.getTitle()); 
+	    values.put(KEY_SOUND2, game.getTitle()); 
+	    values.put(KEY_SOUND3, game.getTitle()); 
+	    values.put(KEY_SOUND4, game.getTitle()); 	   
+	    values.put(KEY_LISTAVAIL, game.getListavail()); 
 	 
 	    // Inserting Row
-	    db.insert(GAMELIST_TABLE_NAME, null, values);
-	    db.close(); // Closing database connection
+	    //NDKBridge.m1db.insert(GAMELIST_TABLE_NAME, null, values);
+	    //NDKBridge.m1db.close(); // Closing database connection
 	}
 }
