6d373e3786cfdb5e1d5a432b426d626943c1bf86
Shaun Thompson <neko68k@gmail.com>
2014-01-01 11:44:24 -0700

    Using MultiSelectSpinner class below. Slight modifications to support db backend, etc.
    https://github.com/wongk/MultiSelectSpinner

--- src/com/neko68k/M1/GameListOptionsActivity.java
+++ src/com/neko68k/M1/GameListOptionsActivity.java
@@ -4,23 +4,30 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class GameListOptionsActivity extends Fragment {
-	private Spinner cpulist;
-	private Spinner sound1list;
-	private Spinner sound2list;
-	private Spinner sound3list;
-	private Spinner sound4list;
-	private Spinner yearlist;
-	private Spinner boardlist;
-	private Spinner mfglist;
+public class GameListOptionsActivity extends Fragment implements OnItemSelectedListener{
+	private MultiSelectSpinner cpulist;
+	private MultiSelectSpinner sound1list;
+	private MultiSelectSpinner sound2list;
+	private MultiSelectSpinner sound3list;
+	private MultiSelectSpinner sound4list;
+	private MultiSelectSpinner yearlist;
+	private MultiSelectSpinner boardlist;
+	private MultiSelectSpinner mfglist;
+	
+	public void onItemSelected(AdapterView<?> a, View v, int i, long l){
+		//CheckBox chk = (CheckBox)v.findViewById(R.id.chkbox);
+		//boolean test = chk.isChecked();
+		return;
+	}
+	public void onNothingSelected(AdapterView<?> a){
+		
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,63 +37,45 @@ public class GameListOptionsActivity extends Fragment {
 				false);
 
 		// get controls
-		cpulist = (Spinner) view.findViewById(R.id.CPU);
-		sound1list = (Spinner) view.findViewById(R.id.Sound1);
-		sound2list = (Spinner) view.findViewById(R.id.Sound2);
-		sound3list = (Spinner) view.findViewById(R.id.Sound3);
-		sound4list = (Spinner) view.findViewById(R.id.Sound4);
-		yearlist = (Spinner) view.findViewById(R.id.Year);
-		mfglist = (Spinner) view.findViewById(R.id.Mfg);
-		boardlist = (Spinner) view.findViewById(R.id.Board);
+		cpulist = (MultiSelectSpinner) view.findViewById(R.id.CPU);
+		sound1list = (MultiSelectSpinner) view.findViewById(R.id.Sound1);
+		sound2list = (MultiSelectSpinner) view.findViewById(R.id.Sound2);
+		sound3list = (MultiSelectSpinner) view.findViewById(R.id.Sound3);
+		sound4list = (MultiSelectSpinner) view.findViewById(R.id.Sound4);
+		yearlist = (MultiSelectSpinner) view.findViewById(R.id.Year);
+		mfglist = (MultiSelectSpinner) view.findViewById(R.id.Mfg);
+		boardlist = (MultiSelectSpinner) view.findViewById(R.id.Board);
+		
+		
+		//boardlist.getItemIdAtPosition(position);
 		
 		SQLiteDatabase db = NDKBridge.m1db.getReadableDatabase();
 		Cursor cpuCursor = GameListOpenHelper.getAllCPU(db);
-		SimpleCursorAdapter sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, cpuCursor,
-				new String[] { "_id","cpu" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		cpulist.setAdapter(sca);
+		cpulist.setItems(cpuCursor, GameListOpenHelper.KEY_CPU);
+
+		//cpulist.setOnItemSelectedListener(this);
 
 		Cursor sound1Cursor = GameListOpenHelper.getAllSound1(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, sound1Cursor,
-				new String[] { "_id","sound1" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		sound1list.setAdapter(sca);
+		sound1list.setItems(sound1Cursor, GameListOpenHelper.KEY_SOUND1);
 
 		Cursor sound2Cursor = GameListOpenHelper.getAllSound2(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, sound2Cursor,
-				new String[] { "_id","sound2" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		sound2list.setAdapter(sca);
+		sound2list.setItems(sound2Cursor, GameListOpenHelper.KEY_SOUND2);
 
 		Cursor sound3Cursor = GameListOpenHelper.getAllSound3(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, sound3Cursor,
-				new String[] { "_id", "sound3" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		sound3list.setAdapter(sca);
+		sound3list.setItems(sound3Cursor, GameListOpenHelper.KEY_SOUND3);
 
 		Cursor sound4Cursor = GameListOpenHelper.getAllSound4(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, sound4Cursor,
-				new String[] { "_id","sound4" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		sound4list.setAdapter(sca);
+		sound4list.setItems(sound4Cursor, GameListOpenHelper.KEY_SOUND4);
 		
 		Cursor mfgCursor = GameListOpenHelper.getAllMfg(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, mfgCursor,
-				new String[] { "_id","mfg" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		mfglist.setAdapter(sca);
+		mfglist.setItems(mfgCursor, GameListOpenHelper.KEY_MFG);
 		
 		Cursor boardCursor = GameListOpenHelper.getAllBoard(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, boardCursor,
-				new String[] { "_id","sys" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		boardlist.setAdapter(sca);
+		boardlist.setItems(boardCursor, GameListOpenHelper.KEY_SYS);
 		
 		Cursor yearCursor = GameListOpenHelper.getAllYear(db);
-		sca = new SimpleCursorAdapter(this.getActivity(),
-				R.layout.option_spinner_layout, yearCursor,
-				new String[] { "_id","year" }, new int[] { R.id.text_id, R.id.text_opt }, 0);
-		yearlist.setAdapter(sca);
+		yearlist.setItems(yearCursor, GameListOpenHelper.KEY_YEAR);
+		
 		
 		db.close();
 		return view;
--- src/com/neko68k/M1/ImportedXML.java
+++
@@ -1,8 +0,0 @@
-package com.neko68k.M1;
-
-// this is the interface for the xml loader in native
-// also the wrapper for sqlite db access
-
-public class ImportedXML {
-
-}
