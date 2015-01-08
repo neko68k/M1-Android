2862c87494c111edad6829e0e491acc99379658f
neko <neko68k@gmail.com>
2015-01-08 09:59:26 -0700

    Migrate to AndroidStudio

---
+++ app/src/main/java/com/neko68k/M1/FileBrowser.java
@@ -0,0 +1,235 @@
+package com.neko68k.M1;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import android.R;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnShowListener;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+
+public class FileBrowser{ 
+	private List<String> directoryEntries = new ArrayList<String>();
+	private File currentDirectory = Environment.getExternalStorageDirectory();
+
+	ArrayAdapter<String> directoryList;
+	Context ctx;
+	
+	AlertDialog dialog;
+	ListView list;
+	
+	public interface FBCallback{
+		public void selected();
+	}
+	
+	public FileBrowser(){
+		
+	}
+	
+	public void showBrowserDlg(final Preference preference){
+    	final AlertDialog.Builder builder = new AlertDialog.Builder(preference.getContext());
+
+    	builder.setTitle("Select Folder");
+    	list = new ListView(preference.getContext());
+    	list.setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int arg2, long arg3) {
+					String selectedFileString = (String) (list.getItemAtPosition(arg2));;
+					if (selectedFileString.equals("(Select this folder)")) {
+						String selectedPath = getCurrent();
+						if(selectedPath!=null){
+							SharedPreferences sp = preference.getSharedPreferences();
+							SharedPreferences.Editor sped = sp.edit();
+							sped.putString(preference.getKey(), selectedPath);
+							sped.commit();	
+							dialog.dismiss();
+							preference.setSummary(selectedPath);
+							if(preference.getKey()!="icondir"){
+								SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(NDKBridge.ctx);
+								Map<String, ?> preferences = prefs.getAll();
+								
+								NDKBridge.basepath = (String) preferences.get("sysdir");
+								NDKBridge.rompath = (String) preferences.get("romdir");
+								NDKBridge.iconpath = (String) preferences.get("icondir");
+								SQLiteDatabase db = NDKBridge.m1db.getWritableDatabase();
+								GameListOpenHelper.wipeTables(db);
+								InitM1Task task = new InitM1Task(NDKBridge.ctx);
+								task.execute();
+							}
+							return;
+						}
+					} else if (selectedFileString.equals("(Go up)")) {
+						upOneLevel();
+					} else {
+						File clickedFile = null;
+						clickedFile = new File(getCurrent()+selectedFileString);		
+						if (clickedFile != null){
+							browseTo(clickedFile);
+						}
+					}
+				}
+    	    });
+    	builder.setView(list);
+    	
+    	list.setAdapter(directoryList);
+
+    	dialog = builder.create();
+    	dialog.show();
+    
+	}
+	
+	public void showBrowserDlg(final String key, final Context ictx){
+		
+		final FBCallback mCallback = (FBCallback)NDKBridge.ctx;
+		
+    	final AlertDialog.Builder builder = new AlertDialog.Builder(ictx);
+
+    	builder.setTitle("Select Folder");
+    	list = new ListView(ictx);
+    	list.setOnItemClickListener(new OnItemClickListener() {
+
+				public void onItemClick(AdapterView<?> arg0, View arg1,
+						int arg2, long arg3) {
+					String selectedFileString = (String) (list.getItemAtPosition(arg2));;
+					if (selectedFileString.equals("(Select this folder)")) {
+						String selectedPath = getCurrent();
+						if(selectedPath!=null){
+							SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ictx);//.getSharedPreferences();
+							SharedPreferences.Editor sped = sp.edit();
+							sped.putString("sysdir", selectedPath+"/m1");
+							sped.putString("romdir",selectedPath+"/m1/roms");
+							sped.putString("icondir",selectedPath+"/m1/icons/");
+							sped.commit();	
+							dialog.dismiss();
+							mCallback.selected();
+							
+							return;
+						}
+					} else if (selectedFileString.equals("(Go up)")) {
+						upOneLevel();
+					} else {
+						File clickedFile = null;
+						clickedFile = new File(getCurrent()+selectedFileString);		
+						if (clickedFile != null){
+							browseTo(clickedFile);
+						}
+					}
+				}
+    	    });
+    	builder.setView(list);
+    	list.setAdapter(directoryList);
+    	
+    	dialog = builder.create();
+    	dialog.show();
+    
+	}
+
+
+	/** Called when the activity is first created. */
+	public FileBrowser(Context ictx) {
+		ctx = ictx;
+		
+		directoryList = new ArrayAdapter<String>(ctx,
+				R.layout.simple_list_item_1, this.directoryEntries);
+
+		browseToRoot();
+	}
+	
+	public String getCurrent(){
+			return (this.currentDirectory.getAbsolutePath());
+	}
+	
+	public String getStringAtOfs(long l){
+		return(this.directoryEntries.get((int) l));
+	}
+	
+	public ArrayAdapter<String> getAdapter(){
+		return(directoryList);
+	}
+
+	/**
+	 * This function browses to the root-directory of the file-system.
+	 */
+	public void browseToRoot() {
+		browseTo(new File("/mnt"));
+	}
+
+	/**
+	 * This function browses up one level according to the field:
+	 * currentDirectory
+	 */
+	public void upOneLevel() {
+		if (this.currentDirectory.getParent() != null)
+			this.browseTo(this.currentDirectory.getParentFile());
+	}
+
+	public void browseTo(final File aDirectory) {
+		
+		File files[] = null;
+		if (aDirectory.isDirectory()) {
+			this.currentDirectory = aDirectory;
+			try {
+				files = aDirectory.listFiles();
+			} catch (Exception e) {
+				Toast.makeText(ctx, "Permission denied.", Toast.LENGTH_SHORT)
+						.show();
+				return;
+			}
+			fill(files);
+
+		}
+	}
+
+	private void fill(File[] files) {
+		this.directoryEntries.clear();
+
+		// Add the "." and the ".." == 'Up one level'
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		this.directoryEntries.add("(Select this folder)");
+		if (this.currentDirectory.getParent() != null)
+			this.directoryEntries.add("(Go up)");
+
+		// On relative Mode, we have to add the current-path to
+						// the beginning
+			int currentPathStringLength = this.currentDirectory
+					.getAbsolutePath().length();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					this.directoryEntries.add(file.getAbsolutePath().substring(
+							currentPathStringLength)
+							+ "/");				
+				}
+			}
+			
+		Collections.sort(this.directoryEntries);
+		directoryList.notifyDataSetChanged();
+	}
+	
+	public List<String> getEntries(){
+		return (directoryEntries);
+	}	
+}
---
+++ app/src/main/java/com/neko68k/M1/InitM1Task.java
@@ -0,0 +1,306 @@
+package com.neko68k.M1;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+public class InitM1Task extends AsyncTask<Void, Void, Void> {
+	ProgressDialog dialog;
+
+	private Context context;
+
+	private Hashtable<Long, String> cpuHashSet = new Hashtable<Long, String>();
+	private Hashtable<Long, String> sound1HashSet = new Hashtable<Long, String>();
+	private Hashtable<Long, String> sound2HashSet = new Hashtable<Long, String>();
+	private Hashtable<Long, String> sound3HashSet = new Hashtable<Long, String>();
+	private Hashtable<Long, String> sound4HashSet = new Hashtable<Long, String>();
+	
+	private Hashtable<Long, String> boardHashSet = new Hashtable<Long, String>();
+	private Hashtable<Long, String> mfgHashSet = new Hashtable<Long, String>();
+	private Hashtable<Long, String> yearHashSet = new Hashtable<Long, String>();
+
+	public InitM1Task(Context c) // pass the context in the constructor
+	{
+		context = c;
+	}
+
+	@Override
+	protected void onPreExecute() {
+		dialog = ProgressDialog.show(context, "",
+				"Initializing. This may take a long time...", true);
+		Log.v("com.neko68.M1", "Starting init...");
+
+	}
+
+	@Override
+	protected Void doInBackground(Void... unused) {
+		String outpath = null;
+		Game game;
+		File xml = new File(NDKBridge.basepath + "/m1.xml");
+		if (xml.exists() == false) {			
+			try {
+				outpath = NDKBridge.basepath;
+				File m1dir = new File(outpath);
+				m1dir.mkdirs();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			Unzip(outpath+"/", R.raw.m1xml20111011);
+
+			try {
+				outpath = NDKBridge.basepath + "/lists/";
+				File m1dir = new File(outpath);
+				m1dir.mkdirs();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			Unzip(outpath, R.raw.lists);
+
+			try {
+				outpath = NDKBridge.basepath + "/roms/";
+				File m1dir = new File(outpath);
+				m1dir.mkdirs();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			try {
+				outpath = NDKBridge.basepath + "/icons/";
+				File m1dir = new File(outpath);
+				m1dir.mkdirs();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		NDKBridge.initM1();
+		// int numGames = NDKBridge.getMaxGames();
+
+		int i = 0;
+		NDKBridge.m1db = new GameDatabaseHelper(context);		
+		// SQLiteDatabase db = NDKBridge.m1db.getWritableDatabase();
+
+		if (!GameListOpenHelper.checkTable()) {
+			game = new Game();
+			CRC32 crc = new CRC32();
+			int maxGames = NDKBridge.getMaxGames();
+			for (i = 0; i < maxGames; i++) {
+				NDKBridge.cur = i;
+
+				game = NDKBridge.queryRom(i);
+				game.romavail = NDKBridge.simpleAudit(i);
+				game.index = i;
+				
+				crc.update(game.mfg.getBytes());
+				mfgHashSet.put(crc.getValue(), game.mfg);
+				game.intmfg = crc.getValue();
+				crc.reset();
+				
+				crc.update(game.sys.getBytes());
+				boardHashSet.put(crc.getValue(), game.sys);
+				game.intsys = crc.getValue();
+				crc.reset();
+				
+				crc.update(game.year.getBytes());
+				yearHashSet.put(crc.getValue(), game.year);
+				game.intyear = crc.getValue();
+				crc.reset();
+				
+				game.soundhw = game.cpu;
+				String soundary[] = game.cpu.split(", ");
+				switch (soundary.length) {
+				case 5:
+					game.setSound4(soundary[4]);
+					crc.update(soundary[4].getBytes());
+					sound4HashSet.put(crc.getValue(), soundary[4]);
+					game.intsound4 = crc.getValue();
+					crc.reset();
+				case 4:
+					game.setSound3(soundary[3]);
+					crc.update(soundary[3].getBytes());
+					sound3HashSet.put(crc.getValue(), soundary[3]);
+					game.intsound3 = crc.getValue();
+					crc.reset();
+				case 3:
+					game.setSound2(soundary[2]);
+					crc.update(soundary[2].getBytes());
+					sound2HashSet.put(crc.getValue(), soundary[2]);
+					game.intsound2 = crc.getValue();
+					crc.reset();
+				case 2:
+					game.setSound1(soundary[1]);
+					crc.update(soundary[1].getBytes());
+					sound1HashSet.put(crc.getValue(), soundary[1]);
+					game.intsound1 = crc.getValue();
+					crc.reset();
+				case 1:
+					game.setCpu(soundary[0]);
+					crc.update(soundary[0].getBytes());
+					cpuHashSet.put(crc.getValue(), soundary[0]);
+					game.intcpu = crc.getValue();
+					crc.reset();
+				case 0:
+					break;
+				}
+				// if(game.romavail==1){
+				GameListOpenHelper.addGame(game);
+				// }
+			}
+
+			GameListOpenHelper.addExtra(GameListOpenHelper.CPU_TABLE, GameListOpenHelper.KEY_CPU, 
+					"!--CPU--!", Long.valueOf(0));
+			GameListOpenHelper.addExtra(GameListOpenHelper.SOUND1_TABLE, GameListOpenHelper.KEY_SOUND1, 
+					"!--Sound Chip 1--! ", Long.valueOf(0));
+			GameListOpenHelper.addExtra(GameListOpenHelper.SOUND2_TABLE, GameListOpenHelper.KEY_SOUND2, 
+					"!--Sound Chip 2--! ", Long.valueOf(0));
+			GameListOpenHelper.addExtra(GameListOpenHelper.SOUND3_TABLE, GameListOpenHelper.KEY_SOUND3, 
+					"!--Sound Chip 3--! ", Long.valueOf(0));
+			GameListOpenHelper.addExtra(GameListOpenHelper.SOUND4_TABLE, GameListOpenHelper.KEY_SOUND4, 
+					"!--Sound Chip 4--! ", Long.valueOf(0));
+			
+			GameListOpenHelper.addExtra(GameListOpenHelper.MFG_TABLE, GameListOpenHelper.KEY_MFG, 
+					"!--Manufacturer--!", Long.valueOf(0));
+			GameListOpenHelper.addExtra(GameListOpenHelper.BOARD_TABLE, GameListOpenHelper.KEY_SYS, 
+					"!--Board--!", Long.valueOf(0));
+			GameListOpenHelper.addExtra(GameListOpenHelper.YEAR_TABLE, GameListOpenHelper.KEY_YEAR, 
+					"!--Year--!", Long.valueOf(0));
+
+			Iterator<?> it = cpuHashSet.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.CPU_TABLE, GameListOpenHelper.KEY_CPU,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			it = sound1HashSet.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.SOUND1_TABLE, GameListOpenHelper.KEY_SOUND1,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			it = sound2HashSet.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.SOUND2_TABLE, GameListOpenHelper.KEY_SOUND2,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			it = sound3HashSet.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.SOUND3_TABLE, GameListOpenHelper.KEY_SOUND3,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			it = sound4HashSet.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.SOUND4_TABLE, GameListOpenHelper.KEY_SOUND4,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			
+			it = mfgHashSet.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.MFG_TABLE, GameListOpenHelper.KEY_MFG,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			
+			it = boardHashSet.entrySet().iterator();
+
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.BOARD_TABLE, GameListOpenHelper.KEY_SYS,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+						
+			it = yearHashSet.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry pairs = (Map.Entry) it.next();
+				GameListOpenHelper.addExtra(GameListOpenHelper.YEAR_TABLE, GameListOpenHelper.KEY_YEAR,
+						(String) pairs.getValue(),
+						(Long) pairs.getKey());
+
+			}
+			
+
+			//db.close();
+		}
+		return (null);
+	}
+
+	@Override
+	protected void onPostExecute(Void result) {
+		
+		dialog.dismiss();
+	}
+
+	private void Unzip(String outpath, int res) {
+		final int BUFFER = 2048;
+		InputStream xmlzip = context.getResources().openRawResource(res);
+		ZipInputStream zis = new ZipInputStream(new BufferedInputStream(xmlzip));
+		BufferedOutputStream dest = null;
+		;
+		ZipEntry entry = null;
+		String finalpath = outpath;
+
+		try {
+			while ((entry = zis.getNextEntry()) != null) {
+				if (entry.isDirectory()) {
+					finalpath = outpath + entry.getName() + "/";
+					File newDir = new File(finalpath);
+					newDir.mkdirs();
+				} else {
+					int count;
+					byte[] data = new byte[BUFFER];
+					FileOutputStream out = new FileOutputStream(outpath
+							+ entry.getName());
+					dest = new BufferedOutputStream(out, BUFFER);
+					while ((count = zis.read(data, 0, BUFFER)) != -1) {
+						dest.write(data, 0, count);
+					}
+					dest.flush();
+					dest.close();
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
