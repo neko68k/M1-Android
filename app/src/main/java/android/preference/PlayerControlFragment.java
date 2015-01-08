044a8399b0a593e3b37103beb56e0116b2602a14
neko68k <neko68k@gmail.com>
2015-01-08 15:57:38 -0700

    created empty classes for new fragments.

---
+++ app/src/main/java/android/preference/PlayerControlFragment.java
@@ -0,0 +1,9 @@
+package android.preference;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by neko on 1/8/15.
+ */
+public class PlayerControlFragment extends Fragment {
+}
---
+++ app/src/main/java/com/neko68k/M1/TrackListFragment.java
@@ -0,0 +1,9 @@
+package com.neko68k.M1;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by neko on 1/8/15.
+ */
+public class TrackListFragment extends ListFragment {
+}
